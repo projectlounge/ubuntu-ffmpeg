248c310f06776061aa0aa5a53bbcff9956a66ff4
Ian Connor <iconnor@projectlounge.com>
2019-11-07 10:17:45 +1000

    Add build script

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+FROM ubuntu:16.04
+
+VOLUME /var/www/client
+VOLUME /var/www/backend
+VOLUME /var/www/frontend
+
+RUN apt-get update
+RUN apt-get upgrade -y
+
+RUN apt-get install -y git zip
+RUN apt-get install -y imagemagick
+RUN apt-get install -y curl
+RUN apt-get install -y wget
+RUN apt-get install -y xz-utils
+RUN apt-get install -y ca-certificates
+RUN apt-get install -y apache2
+RUN apt-get install -y nano
+
+# ffmpeg setup
+COPY ./build-ffmpeg.ubuntu.sh /usr/local/bin/build-ffmpeg
+RUN chmod +x /usr/local/bin/build-ffmpeg
+RUN build-ffmpeg
+
+# install php
+RUN apt-get install -y php7.0 \
+	php7.0-cli \
+	php7.0-common \
+	php7.0-curl \
+	php7.0-fpm \
+	php7.0-json \
+	php7.0-mbstring \
+	php7.0-mcrypt \
+	php7.0-mysql \
+	php7.0-opcache \
+	php7.0-xml \
+	php7.0-zip \
+    php7.0-dom \
+	php7.0-gd \
+    php-xdebug \
+    libapache2-mod-php7.0
---
+++ build-ffmpeg.ubuntu.sh
@@ -0,0 +1,99 @@
+# Script to build FFMPEG with FDK-AAC, H264 and LAME MP3
+# Reference: https://trac.ffmpeg.org/wiki/CompilationGuide/Centos
+
+# Install dependancies and create directory
+apt-get update -qq && apt-get -y install \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
+  git-core \
+  libass-dev \
+  libfreetype6-dev \
+  libsdl2-dev \
+  libtool \
+  libva-dev \
+  libvdpau-dev \
+  libvorbis-dev \
+  libxcb1-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  pkg-config \
+  texinfo \
+  wget \
+  zlib1g-dev \
+  bzip2
+
+mkdir -p ~/ffmpeg_sources ~/bin
+
+# Install NASM (asembler)
+cd ~/ffmpeg_sources && \
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.13.03/nasm-2.13.03.tar.bz2 && \
+tar xjvf nasm-2.13.03.tar.bz2 && \
+cd nasm-2.13.03 && \
+./autogen.sh && \
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
+make && \
+make install
+
+# Install Yasm (assembler)
+cd ~/ffmpeg_sources && \
+wget -O yasm-1.3.0.tar.gz https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz && \
+tar xzvf yasm-1.3.0.tar.gz && \
+cd yasm-1.3.0 && \
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
+make && \
+make install
+
+# Install libx264
+cd ~/ffmpeg_sources && \
+git -C x264 pull 2> /dev/null || git clone --depth 1 https://git.videolan.org/git/x264 && \
+cd x264 && \
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic && \
+PATH="$HOME/bin:$PATH" make && \
+make install
+
+# Install libfdk-aac
+cd ~/ffmpeg_sources && \
+git -C fdk-aac pull 2> /dev/null || git clone --depth 1 https://github.com/mstorsjo/fdk-aac && \
+cd fdk-aac && \
+autoreconf -fiv && \
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared && \
+make && \
+make install
+
+# Install libmp3lame
+cd ~/ffmpeg_sources && \
+wget -O lame-3.100.tar.gz https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz && \
+tar xzvf lame-3.100.tar.gz && \
+cd lame-3.100 && \
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm && \
+PATH="$HOME/bin:$PATH" make && \
+make install
+
+# Install ffmpeg
+cd ~/ffmpeg_sources
+curl -O -L https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar xjvf ffmpeg-snapshot.tar.bz2
+cd ffmpeg
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+  --prefix="$HOME/ffmpeg_build" \
+  --pkg-config-flags="--static" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" \
+  --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --extra-libs="-lpthread -lm" \
+  --bindir="$HOME/bin" \
+  --enable-gpl \
+  --enable-libfdk-aac \
+  --enable-libmp3lame \
+  --enable-libx264 \
+  --enable-nonfree && \
+PATH="$HOME/bin:$PATH" make && \
+make install && \
+hash -r
+
+ln -sf ~/ffmpeg_sources/ffmpeg/ffmpeg /usr/bin/ffmpeg
+ln -sf ~/ffmpeg_sources/ffmpeg/ffprobe /usr/bin/ffprobe
+
+chmod 755 ~
+chmod -R 755 ~/ffmpeg_sources/ffmpeg
